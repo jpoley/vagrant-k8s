34dc07e2e6b298afe1a598d4536351d6d745b550
jason poley <jason.poley@gmail.com>
2022-12-22 14:07:06 -0500

    updates

--- Vagrantfile
+++ Vagrantfile
@@ -13,9 +13,12 @@ Vagrant.configure("2") do |config|
         master.vm.box = IMAGE_NAME
         master.vm.network "private_network", ip: "192.168.56.10"
         master.vm.hostname = "k8s-master"
-	master.vm.provision "file", source: "./kubernetes-setup/kube-flannel.yml", destination: "kube-flannel.yml"
+        
+	    master.vm.provision "file", source: "./kubernetes-setup/kube-flannel.yml", destination: "kube-flannel.yml"
+        master.vm.provision "file", source: "./kubernetes-setup/calico.yml", destination: "calico.yml"
         master.vm.provision "file", source: "./kubernetes-setup/tigera-operator.yaml", destination: "tigera-operator.yaml"
         master.vm.provision "file", source: "./kubernetes-setup/custom-resources.yaml", destination: "custom-resources.yaml"
+
         master.vm.provision "file", source: "./kubernetes-setup/get-etcdctl.sh", destination: "get-etcdctl.sh"
         master.vm.synced_folder "yaml/",  "/home/vagrant/yaml/"
         master.vm.provision "ansible" do |ansible|
--- yaml/argo.sh
+++ yaml/argo.sh
@@ -1,19 +1,26 @@
 
 kubectl create ns argo
+wget https://github.com/argoproj/argo-workflows/releases/download/v3.3.10/install.yaml
+mv install.yaml argo-workflow.yaml
 kubectl apply -n argo -f argo-workflow.yaml
-# https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml
 
 kubectl create namespace argo-events
+wget https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install.yaml
+mv install.yaml argo-events.yaml
 kubectl apply -f argo-events.yaml
-# https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install.yaml
+
+
+wget https://raw.githubusercontent.com/argoproj/argo-events/stable/examples/eventbus/native.yaml
+mv native.yaml eventbus.yaml
 kubectl apply -n argo-events -f eventbus.yaml
-# https://raw.githubusercontent.com/argoproj/argo-events/stable/examples/eventbus/native.yaml
+
 
 kubectl create namespace argocd
+wget https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+mv install.yaml argocd.yaml
 kubectl apply -n argocd -f argocd.yaml
-# https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 # install argocd
