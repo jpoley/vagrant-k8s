29ad8bac9532eeb55eef4544e7f11272cf10923c
jason poley <jason.poley@gmail.com>
2022-12-24 09:33:54 -0500

    newer gcp image

--- CKS/gcp/gcp-create-vm.sh
+++ CKS/gcp/gcp-create-vm.sh
@@ -1,5 +1,5 @@
 gcloud compute instances create $1 --zone=us-east1-c \
 --machine-type=e2-medium \
---image=ubuntu-1804-bionic-v20201014 \
+--image=ubuntu-2004-focal-v20221213 \
 --image-project=ubuntu-os-cloud \
 --boot-disk-size=50GB
