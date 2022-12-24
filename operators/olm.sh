16b73023289c50341e14f432ebae8d0fb8e9f487
jason poley <jason.poley@gmail.com>
2022-12-24 09:50:15 -0500

    updating olm

--- operators/olm.sh
+++ operators/olm.sh
@@ -1,4 +1,4 @@
-export olm_release=0.12.0
+export olm_release=0.22.0
 kubectl apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${olm_release}/crds.yaml
 kubectl apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${olm_release}/olm.yaml
 
