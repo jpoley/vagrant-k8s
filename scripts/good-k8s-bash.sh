9a787fb720f6b994bd6eaa59e6e427dd7280acd2
jason poley <jason.poley@gmail.com>
2022-12-24 10:45:10 -0500

    add helper + remove yaml

---
+++ scripts/good-k8s-bash.sh
@@ -0,0 +1,5 @@
+source <(kubectl completion bash) # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
+echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+
+alias k=kubectl
+complete -o default -F __start_kubectl k
---
+++ scripts/good-k8s-zsh.sh
@@ -0,0 +1,5 @@
+source <(kubectl completion zsh)  # set up autocomplete in zsh into the current shell
+echo '[[ $commands[kubectl] ]] && source <(kubectl completion zsh)' >> ~/.zshrc # add autocomplete permanently to your zsh shell
+
+alias k=kubectl
+complete -o default -F __start_kubectl k
