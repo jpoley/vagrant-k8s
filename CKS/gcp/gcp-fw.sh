c758b3467804330c8c9620db06831a1eda967f1f
jason poley <jason.poley@gmail.com>
2022-12-24 09:45:40 -0500

    fw refresh

--- CKS/gcp/gcp-fw.sh
+++ CKS/gcp/gcp-fw.sh
@@ -1,3 +1,4 @@
+gcloud compute firewall-rules delete nodeports
 MY_IP=$(curl http://ipinfo.io/ip)
 gcloud compute firewall-rules create nodeports --allow tcp:30000-40000 --source-ranges="$MY_IP/32"
 
