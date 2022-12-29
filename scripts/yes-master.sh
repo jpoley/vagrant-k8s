e441201ed07041acb34cf741cd7eb75c77fd8e89
jason poley <jason.poley@gmail.com>
2022-12-28 22:39:19 -0500

    adding

--- scripts/yes-master.sh
+++ scripts/yes-master.sh
@@ -1,1 +1,2 @@
-kubectl taint nodes $(hostname) node-role.kubernetes.io/master:NoSchedule
+kubectl taint nodes $(hostname) node-role.kubernetes.io/master:NoSchedule-
+kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane:NoSchedule-
